7c835d56280a7d698a55c46638190e2ba0f0fa93
Adam Crespo <alc49@LAPTOP-7KRCGAGF.localdomain>
2022-05-31 13:28:52 -0400

    styling changes

--- client/src/Components/Services.js
+++ client/src/Components/Services.js
@@ -16,23 +16,35 @@ return(
         <img className="home-title" src={logo} alt="logo"/>
         
         <div className="services">
-            <div className='anxiety-info'>
+            <div className='first'>
             <img style={{height: '300px', width: '400px', marginLeft: "20%"}} src={services1} alt="your-feelings-matter" />
+            <div className='anxiety-info'>
                 <p><strong>Anxiety and Panic Reduction:</strong> Constantly anxious? Do you experience panic attacks? Learn how to implement effective techniques that help to reduce anxiety and panic, increase your control.</p>
+                <br></br>
                 <p><strong>Bereavement Therapy:</strong> Grieving the loss of a loved one? We offer specific services for effective coping, and techniques for moving forward.</p>
-        
+            </div>
+            </div>
+            <div className='first'>
             <img style={{height: '300px', width: '400px', marginLeft: "20%"}} src={services2} alt="therapy-in-session" />
+            <div className='anxiety-info'>
                 <p><strong>Depression Allevation:</strong> Experiencing depression, hopelessness, or despair? We can help you learn how to view life in a more positive manner through proven techniques to improve your mood.</p>
                 <p><strong>Career and Vocational Counseling:</strong> Are you dissatisfied with work and want to make a change? Our vocational counseling services can you establish goals and assist you in achieving them.</p>
+            </div>
+            </div>
             
-            
+            <div className='first'>
             <img style={{height: '300px', width: '400px', marginLeft: "20%"}} src={services3} alt="children" />
+            <div className='anxiety-info'>
                 <p><strong>Child and Adolescent Services:</strong> Many adolescent issues can be similar to those affecting adults, but many are age-specific and unique to their stage of development.</p>
                 <p><strong>Marital, Relationship and Family Problems:</strong> Need help improving your relationship? Individuals, couples or families can use this service to help identify problem areas and how to communicate more effectively.</p>
-            
+                </div>    
+            </div>
+            <div className='first'>
             <img style={{height: '300px', width: '400px', marginLeft: "20%"}} src={services4} alt="surgical-recovery" />
+            <div className='anxiety-info'>
                 <p><strong>Pre- and Post-Surgical Counseling:</strong> Research has shown that both pre and post-surgical counseling, can help individuals emotionally as well as having a positive impact on their physical health/surgical outcome.</p>
                 <p><strong>Telephone and Video Sessions:</strong> With COVID-19 and other barriers that may keep you from visiting in-person, it's imperative you can still receive the help you need. We offer telephone and video sessions as a viable alternative.</p>
+                </div>    
             </div>
         </div>
     </>
